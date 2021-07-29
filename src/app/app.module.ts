97200943eefd3e867306b50047e7aa71aebd3371
Josiel Faria <developerjosiel@gmail.com>
2021-07-29 12:57:41 -0300

    Adicionando select icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,29 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AngularMaterialModule } from './angular-material/angular-material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChangeIconDialog } from './components/change-icon/change-icon.dialog';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { IconNamesPipe } from './shared/pipes/icon-names.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    NotFoundPageComponent
+    NotFoundPageComponent,
+    ChangeIconDialog,
+    IconNamesPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
     AngularMaterialModule,
     NgbModule,
   ],
