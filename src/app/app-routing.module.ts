2841c7d8169ad20392d2a173f4f559961c093585
Josiel Faria <developerjosiel@gmail.com>
2021-08-03 15:09:02 -0300

    Refatorando projeto, removendo recurso nao utilizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { UploadImagePageComponent } from './pages/upload-image-page/upload-image-page.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomePageComponent },
+  { path: '', redirectTo: '/images', pathMatch: 'full' },
   { path: 'images', component: UploadImagePageComponent },
   { path: '**', component: NotFoundPageComponent } // deixar esse sempre por ultimo
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AngularMaterialModule } from './angular-material/angular-material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChangeIconDialog } from './components/change-icon/change-icon.dialog';
-import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { UploadImagePageComponent } from './pages/upload-image-page/upload-image-page.component';
-import { IconNamesPipe } from './shared/pipes/icon-names.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IconNamesPipe,
-    ChangeIconDialog,
     NotFoundPageComponent,
-    HomePageComponent,
     UploadImagePageComponent
   ],
   imports: [
